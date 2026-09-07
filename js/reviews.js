06821d611453140cb010dea5beb180309a51e3c2
Mark Mayne <markmayne@ymail.com>
2026-09-07 12:42:04 -0400

    Add approved reviews data

---
+++ js/reviews.js
@@ -0,0 +1,8 @@
+window.approvedReviews = [
+  {
+    "name": "Emmy",
+    "rating": null,
+    "date": "",
+    "review": "From the moment we started chatting with her, we knew that Katelyn would likely be the one. Her gentle nature and kind spirit drew us right in. She has been a great communicator and made the entire process so easy and stress free. Katelyn is kind, patient, organized, and a great communicator. We couldn’t recommend working with Katelyn enough."
+  }
+];
